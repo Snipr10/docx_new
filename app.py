1f68bce7e90085c54f85cab8c0e81c2d91c4f1ea
oleg <gusevoleg@gmail.com>
2022-02-02 19:38:23 +0300

    fixed

--- app.py
+++ app.py
@@ -1191,8 +1191,6 @@ def add_table_tonal(document, chart_title_type_, chart_number, statistic_chart_t
 
 
 if __name__ == "__main__":
-    index()
-
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
